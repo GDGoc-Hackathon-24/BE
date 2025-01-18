77e283513d5cc0f3ba3bb83fb8b3d80027440c19
sangeun <olsang1203@gmail.com>
2025-01-18 19:52:18 +0900

    feat: 서버 확인용 healthcheck 추가

---
+++ src/main/java/GDGoC/team_24/HealthCheckController.java
@@ -0,0 +1,12 @@
+package GDGoC.team_24;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+}
