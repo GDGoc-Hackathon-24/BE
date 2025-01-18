15bc0868328d0e8efcae7cb7ae2cf46a9135edd5
sangeun <olsang1203@gmail.com>
2025-01-19 01:06:40 +0900

    refactor: Handler 디렉토리 이동

--- src/main/java/GDGoC/team_24/global/exception/Handler/S3Handler.java
+++ src/main/java/GDGoC/team_24/global/exception/Handler/S3Handler.java
@@ -1,6 +1,7 @@
-package GDGoC.team_24.global.exception;
+package GDGoC.team_24.global.exception.Handler;
 
 import GDGoC.team_24.global.code.BaseErrorCode;
+import GDGoC.team_24.global.exception.GeneralException;
 
 public class S3Handler extends GeneralException {
     public S3Handler(BaseErrorCode errorCode) {
